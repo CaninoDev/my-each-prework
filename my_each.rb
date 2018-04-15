793d5e51581d86fc12067ecca085da245f6f5d27
Canino Developments <ian.canino@gmail.com>
2018-04-15 11:31:26 -0400

    implemented an arr.collect like method

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(anArray)
+  if block_given?
+    i = 0
+    while i < anArray.length
+      yield(anArray[i])
+      i = i + 1
+    end
+  end
+
+  anArray
+
 end
